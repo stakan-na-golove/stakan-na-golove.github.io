5fd41f59546ddb077ed4f74165ff627c2d22e995
stakan-na-golove <TimG0@yandex.ru>
2026-04-30 18:56:23 +0300

    Add GitHub Actions deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/stakan-na-golove.github.io/', // 👈 Важно!
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
